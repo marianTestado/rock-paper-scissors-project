d797414834670092f7751bf6b358790d7b25f20b
marianTestado <149865048+marianTestado@users.noreply.github.com>
2023-12-15 19:48:42 -0300

    Added best of 5 game functionality.

--- js/javascript.js
+++ js/javascript.js
@@ -24,7 +24,7 @@ function getComputerChoice(){
 
 }
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection,){
 
     let playerAnswer = playerSelection.toLowerCase(playerSelection);
 
@@ -33,7 +33,8 @@ function playRound(playerSelection, computerSelection){
 
     if(playerAnswer == computerSelection)
     {
-        return `You both chose ${playerAnswer.replace(playerAnswer.charAt(0), (playerAnswer.charAt(0)).toUpperCase() )}, it's a draw!`;
+        console.log(`You both chose ${playerAnswer.replace(playerAnswer.charAt(0), (playerAnswer.charAt(0)).toUpperCase() )}, it's a draw!`);
+        return 0;
     }
     else
     {
@@ -42,48 +43,91 @@ function playRound(playerSelection, computerSelection){
             case 'rock':
                 if(computerSelection == 'paper')
                 {
-                    return 'Paper beat Rock, you lose!';
+
+                    console.log('Paper beat Rock, you lose!');
+                    return 'computer';
                 }
                 else
                 {
-                    return 'Rock beat Scissors, you win!';
+                    console.log('Rock beat Scissors, you win!');
+                    return 'player';
                 }
                 break;
             
             case 'paper':
                 if(computerSelection == 'scissors')
                 {
-                    return 'Scissors beat Paper, you lose!';
+                    console.log('Scissors beat Paper, you lose!');
+                    return 'computer';
                 }
                 else
                 {
-                    return 'Paper beat Rock, you win!';
+                    console.log('Paper beat Rock, you win!');
+                    return 'player';
                 }
                 break;
 
             case 'scissors':
                 if(computerSelection == 'rock')
                 {
-                    return 'Rock beat Scissors, you lose!';
+                    console.log('Rock beat Scissors, you lose!');
+                    return 'computer';
                 }
                 else
                 {
-                    return 'Scissors beat Paper, you win!';
+                    console.log('Scissors beat Paper, you win!');
+                    return 'player';
                 }
                 break;
 
             default:
-                return 'You entered an invalid choice, choose rock, paper or scissors.'
-                break;
-            
+                console.log('You entered an invalid choice, choose between Rock, Paper or Scissors.');
+                return 0;
+                break;   
         }
     }
+}
+
+function game(){
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+
+    do{
+
+        let playerChoice = prompt('Choose Rock, Paper or Scissors.');
+        let computerChoice = getComputerChoice();
+
+        switch(playRound(playerChoice, computerChoice))
+        {
+            case 'player':
+                playerScore++;
+                break;
+
+            case 'computer':
+                computerScore++;
+                break;
+        }
+
+        console.log(`Player Score: ${playerScore}`);
+        console.log(`Computer Score: ${computerScore}`);
+
+    }while(playerScore < 3 && computerScore < 3)
+
+    if(playerScore == 3)
+    {
+        return 'Congratulations, you won!';
+    }
+    else
+    {
+        return 'You lose, try again!';
+    }
+    
 
 
 
 
 }
 
-const playerSelection = 'pApEr';
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+console.log(game());
